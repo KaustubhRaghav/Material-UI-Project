3d00e321a3a9ed68cece14a9a646d3cac97a2773
Kaustubh Raghav <kaustubhraghav267@gmail.com>
2023-05-14 08:28:28 +0530

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Container } from "@mui/material";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Container sx={{ backgroundColor: "red" }}>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </Container>
+  );
 }
 
 export default App;
